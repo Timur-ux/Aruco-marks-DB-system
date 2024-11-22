4ce47cd88779a7874c8764f4625499e011aa5336
Timur <timur.buchkin@mail.ru>
2024-11-23 01:04:14 +0300

    add flask backend start to docker compose file

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,9 @@ from flask import Flask
 
 app = Flask(__name__);
 
+@app.route("/")
+def index():
+    return "Hello vova"
 @app.route('/api/login', methods=["GET"])
 def auth():
     return "auth result"
@@ -16,4 +19,4 @@ def processMark(mark_id):
     return f"get data of mark with id: {mark_id}"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
