61862e8c6263115af6307ec6e02293f83a290f82
Timur <timur.buchkin@mail.ru>
2024-11-22 12:54:30 +0300

    add base request responce functional, TODO: dockerize

---
+++ backend/app.py
@@ -0,0 +1,19 @@
+#!flask/bin/python
+from flask import Flask
+
+app = Flask(__name__);
+
+@app.route('/api/login', methods=["GET"])
+def auth():
+    return "auth result"
+
+@app.route('/api/marks', methods=["GET"])
+def getMarksList():
+    return "get marks list result"
+
+@app.route('/api/marks/<int:mark_id>', methods=["GET", "POST", "PUT", "DELETE"])
+def processMark(mark_id):
+    return f"get data of mark with id: {mark_id}"
+
+if __name__ == "__main__":
+    app.run(debug=True)
