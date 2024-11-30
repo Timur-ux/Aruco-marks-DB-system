b06521e91ca23c4c579bd05d772cec9fef2a8d9e
Timur <timur.buchkin@mail.ru>
2024-11-30 17:47:53 +0300

    add auth via post request

--- frontend/src/service/authFunc.js
+++ frontend/src/service/authFunc.js
@@ -2,11 +2,11 @@ import { client } from "./client"
 import { getSHA256Hash } from "boring-webcrypto-sha256"
 
 export const authFunc = access => async (login, password) => {
-  const response = await client.get("/api/login", {params: {
+  const response = await client.post("/api/login",  {
     access: access.toLowerCase(),
     login: login,
     password: await getSHA256Hash(password),
-  }})
+  });
 
 
   return response;
